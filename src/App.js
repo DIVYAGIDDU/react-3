2a0d9f863a0d39a8feb404555f21d17ca2f914f4
divya <divyagiddu123@gmail.com>
2022-10-19 06:29:24 -0700

    okay

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Index from './Com[onents/index';
 
 function App() {
   return (
     <div>
+      <Index/>
       <h1>Heelo</h1>
         </div>
   );
