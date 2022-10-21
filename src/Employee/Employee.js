483eee7cf26f43fbc13f3dcc4cd9233da2b4d97f
divya <divyagiddu123@gmail.com>
2022-10-21 07:24:07 -0700

    okay

--- src/Employee/Employee.js
+++ src/Employee/Employee.js
@@ -2,8 +2,10 @@ import React from "react";
 import Employees from "./data";
 class Employee extends React.Component{
     render(){
+        
         return<div className="container md-5">
             <pre>{JSON.stringify(Employee)}</pre>
+            <input>Search here</input>
             <div className="row">
             <div className="col-md-6">
             <div className="table table-hover">
